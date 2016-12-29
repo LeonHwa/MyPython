c82465849c9fc169e7631192c6a62143c4e05a86
LeonHwa <hwestseacoast@163.com>
2016-12-30 00:02:41 +0800

    修改布局
    修改布局

--- WebApp/handlers.py
+++ WebApp/handlers.py
@@ -21,7 +21,10 @@ def index(request):
     blogs = [
         Blog(id='1', name='Test Blog', summary=summary, created_at=time.time()-120),
         Blog(id='2', name='Something New', summary=summary, created_at=time.time()-3600),
-        Blog(id='3', name='Learn Swift', summary=summary, created_at=time.time()-7200)
+        Blog(id='3', name='Learn Swift', summary=summary, created_at=time.time()-7200),
+        Blog(id='4', name='Learn Swift', summary=summary, created_at=time.time() - 7200),
+        Blog(id='5', name='Learn Swift', summary=summary, created_at=time.time() - 7200),
+        Blog(id='6', name='Learn Swift', summary=summary, created_at=time.time() - 7200),
     ]
     # jinja2
     return {
