996a4765bfc4027bd6cdac62c66485440022d833
LeonHwa <Leon.Hwa@hillbily.local>
2017-01-19 18:35:31 +0800

    tab切换
    tab切换

--- WebApp/www/static/js/base.js
+++ WebApp/www/static/js/base.js
@@ -5,12 +5,20 @@
 
 
 $(function () {
-    
+    function addCircle (name) {
+      var act = document.getElementsByClassName(name)[0];
+      var para = document.createElement("div");
+      para.style.cssText = "border-radius: 50%; width: 6px;height: 6px; background: #c8c8c8;  display: inline-block; ";
+      para.className = "circle";
+      act.appendChild(para);
+    }
+    addCircle('active');
     $('.nav-collapse ul li').click(function () {
-        
+        $('.circle').remove();
         $('.nav-collapse ul li').each(function () {
-             $(this).removeClass('active');
+            $(this).removeClass('active');
         })
         $(this).addClass('active');
+         addCircle('active');
     })
 })
